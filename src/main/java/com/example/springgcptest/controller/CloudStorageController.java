6b9b56d68bf6cbfc201e2a31cdc2b3702643b64b
黃越 <moontea306@gmail.com>
2022-05-30 21:43:16 +0800

    add RequestPart

--- src/main/java/com/example/springgcptest/controller/CloudStorageController.java
+++ src/main/java/com/example/springgcptest/controller/CloudStorageController.java
@@ -1,11 +1,14 @@
 package com.example.springgcptest.controller;
 
 import com.example.springgcptest.service.CloudStorageService;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.MediaType;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +22,7 @@ import java.nio.charset.StandardCharsets;
 @RestController
 public class CloudStorageController {
 
+
     public CloudStorageController(HttpServletResponse response, CloudStorageService cloudStorageService) {
         this.response = response;
         this.cloudStorageService = cloudStorageService;
@@ -28,8 +32,8 @@ public class CloudStorageController {
 
     private CloudStorageService cloudStorageService;
 
-    @PostMapping("/upload")
-    public String upload(MultipartFile file) {
+    @PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String upload(@RequestPart MultipartFile file) {
         try {
             cloudStorageService.upload(file);
             return "success";
